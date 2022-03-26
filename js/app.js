0a06bdb0701fb33e0d2ff420234421d0fe967935
crescentpartha <crescentpartha406@gmail.com>
2022-03-26 18:31:16 +0600

    Modal added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,66 @@
+function modalFunction(myModal, myBtn, serial) {
+    // Get the modal
+    var modal = document.getElementById(myModal);
+
+    // Get the button that opens the modal
+    var btn = document.getElementById(myBtn);
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[serial];
+    // var span = document.getElementsByClassName("close");
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+const konik = () => {
+    // alert(`
+    // Name: Provakor Das
+    // ID: 272
+    // Masters in Information Technology (MIT)
+    // Shahjalal University of Science and Technology (SUST)
+    // `);
+    modalFunction('myModal1', 'myBtn1', 0);
+}
+
+const partha = () => {
+    // let result = confirm(`
+    // Name: Crescent Deb Nath Partha
+    // ID: 223
+    // Masters in Information Technology (MIT)
+    // Shahjalal University of Science and Technology (SUST)
+    
+    // Do you want to see as a modal?
+    // `);
+    // if (result === true) {
+    //     modalFunction('myModal2', 'myBtn2', 1);
+    // }
+    modalFunction('myModal2', 'myBtn2', 1);
+}
+
+const showmen = () => {
+    // alert(`
+    // Name: Showmen Purkayastha
+    // ID: 230
+    // Masters in Information Technology (MIT)
+    // Shahjalal University of Science and Technology (SUST)
+    // `);
+    modalFunction('myModal3', 'myBtn3', 2);
+}
 
 // add clear button alert
 
@@ -37,4 +100,5 @@ document.getElementById('submit').addEventListener('click', function(){
         // messageField.value = '';
     }
 });
+
 
